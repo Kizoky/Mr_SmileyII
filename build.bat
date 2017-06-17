087034350cc1b0a9668d4905b3c620037d5d9bc7
Kizoky <kilerzolika@gmail.com>
2017-06-17 03:54:21 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-tools\7za a -tzip -mm=Deflate -mx=5 -x!"*.git*" -x!"maps\*.backup*" -x!"*graphics\*.db*" -x!"*textures\*.db*" -x!"*patches\*.db*" -x!"*sprites\*.db*" -x!"*\.bak*" -x!"tools" -x!"source\UDMF*" -x!"build.bat" -x!"*.pk3" Mr_SmileyII.pk3 *
+tools\7za a -tzip -mm=Deflate -mx=5 -x!"*.git*" -x!"maps\*.backup*" -x!"*graphics\*.db*" -x!"*textures\*.db*" -x!"*patches\*.db*" -x!"*sprites\*.db*" -x!"*\.bak*" -x!"tools" -x!"source\UDMF*" -x!"build.bat" -x!"*.pk3" Mr_SmileyII_ZANDRO.pk3 *
